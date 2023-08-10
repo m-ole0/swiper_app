0d63b4686e39d50d71e54f88c236836d97cf0c09
m-ole0 <138345360+m-ole0@users.noreply.github.com>
2023-08-11 01:10:02 +0900

    [Finish]カリキュラム13

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,13 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+// jqueryをインポートする。
+import "jquery"
 // 下の記述で作成したscript.jsファイルを読み込む。
 import "script.js"
+
+import Swiper from 'swiper/swiper-bundle.js';
+import 'swiper/swiper-bundle.css';
 
 Rails.start()
 Turbolinks.start()
